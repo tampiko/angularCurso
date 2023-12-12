0dc7266c66f374ba71185525d6b067cfe85ca92b
Juan Manuel Herrera Carrera <tampiko@gmail.com>
2023-12-12 08:12:22 -0600

    Creando los componentes

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TareasComponent } from './tareas/tareas.component';
+
+
+
+@NgModule({
+  declarations: [
+    TareasComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PagesModule { }
