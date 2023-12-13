a60c4ceb8f6fa7bec3785bbfd836266d40b625f6
Juan Manuel Herrera Carrera <tampiko@gmail.com>
2023-12-12 23:28:10 -0600

    Agergando Detalles
    Se agrego el fontawesome4, se limpio el navbar.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListadoComponent } from './pages/listado/listado.component';
 
-const routes: Routes = [{ path: 'listado', component: ListadoComponent }];
+const routes: Routes = [
+  { path: 'listado', component: ListadoComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'listado' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
